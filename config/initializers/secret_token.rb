0153295826e469dcc2aa1e9cb9e46544a8c83b85
Aaron Patterson <aaron.patterson@gmail.com>
2013-08-08 16:18:06 +0900

    adding ko1 test app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Ko1TestApp::Application.config.secret_key_base = '723a995ce38cb724a0d582afa889e76d0155e6984e4d74504519c78d204d1aeac18a74fb2b0dacc7769cda10e3728153b5910d50a669a854cb9ec6592d651bde'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Ko1TestApp::Application.config.session_store :cookie_store, key: '_ko1-test-app_session'
